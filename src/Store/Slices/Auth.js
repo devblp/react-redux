659857b574bfac51db5e313023f1e0444bd8fb3e
BlP. <dev.sinanp@gmail.com>
2024-05-26 14:39:02 -0700

    create store files

--- src/Store/Slices/Auth.js
+++ src/Store/Slices/Auth.js
@@ -1,20 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    user:{},
-    token: null,
-}
+  user: {},
+  token: null,
+};
 
 const authSlice = createSlice({
-    name: 'auth',
-    initialState,
-    reducers:{
-        login(state, action) {
-            state.user = action.payload.user
-            state.token = action.payload.token
-        }
-    }
-})
+  name: "auth",
+  initialState,
+  reducers: {
+    login: (state, action) => {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+    },
+  },
+});
 
-export const {login} = authSlice.actions;
-export default authSlice.reducer
+export const { login } = authSlice.actions;
+export default authSlice.reducer;
